2e93c3b6410d27fc01206123bda93abe1c97f959
Martin <martinandrovich@gmail.com>
2022-03-26 13:31:17 +0100

    code revision - convert identation to tabs > indent with tab, align with space - remove comment '# added' from CMakeLists.txt - remove kd, kp scalars - load init pos from config > remove Q_D_INIT - load gains from config - load num_joints dynamically - organize/group code in .h, make things private - change '!' to 'not' for readability - init() > remove shadow copies of arm_id + joint_names - change ROS_ERROR("franka_controller..) to use CONTROLLER_NAME - ptr/ref convention, 'auto* name' and 'auto& name', and not 'auto *name' etc - change to const auto& for-loop when loading joint handles - Eigen::Matrix7x7 > Eigen::Matrix7d (convention) - change to Kp, Kd matrices - qdot > qd, tau_des > tau_d, dQ > dq - simplify get_() functions + remove unused ones - remove launch files > duplicates, use teleop_grasp robot.launch instead - add $(arg arm_id) in config file - fix includes + dependencies in CMakeLists + package.xml - add authors, fix description

--- franka_controllers/include/franka_controllers/joint_position_pd_gravity_controller.h
+++ franka_controllers/include/franka_controllers/joint_position_pd_gravity_controller.h
@@ -4,87 +4,72 @@
 #include <Eigen/Eigen>
 
 #include <ros/ros.h>
-#include <controller_interface/controller.h>
-#include <hardware_interface/joint_command_interface.h>
+#include <controller_interface/multi_interface_controller.h>
 #include <pluginlib/class_list_macros.h>
 #include <realtime_tools/realtime_buffer.h>
-#include <std_msgs/Float64MultiArray.h>
-#include <urdf/model.h>
-#include <sensor_msgs/JointState.h>
+#include <hardware_interface/joint_command_interface.h>
 #include <franka_hw/franka_model_interface.h>
 #include <franka_hw/franka_state_interface.h>
-#include <controller_interface/multi_interface_controller.h>
+#include <std_msgs/Float64MultiArray.h>
 
 namespace Eigen
 {
-    using Vector7d  = Eigen::Matrix<double,7,1>;
-    using Matrix7x7 = Eigen::Matrix<double,7,7>;
-    using Matrix6x7 = Eigen::Matrix<double,6,7>;
+	using Vector7d  = Eigen::Matrix<double,7,1>;
+	using Matrix7d  = Eigen::Matrix<double,7,7>;
 }
 
 namespace franka_controllers
 {
-    class JointPositionPDGravityController final : public controller_interface::MultiInterfaceController<franka_hw::FrankaModelInterface,
-                                                                                                         hardware_interface::EffortJointInterface,
-                                                                                                         franka_hw::FrankaStateInterface>
-    {
-    public:
-        static inline constexpr auto CONTROLLER_NAME     = "JointPositionPDGravityController";
-        static inline constexpr auto SATURATE_ROTATUM    = true;
-        static inline constexpr auto TAU_DOT_MAX         = 1000.;
-        static inline const std::vector<double> Q_D_INIT = {0, 0, 0, -1.57079632679, 0, 1.57079632679, 0.785398163397}; // taken from panda.launch
+	class JointPositionPDGravityController final : public controller_interface::MultiInterfaceController<franka_hw::FrankaModelInterface,
+	                                                                                                     hardware_interface::EffortJointInterface,
+	                                                                                                     franka_hw::FrankaStateInterface>
+	{
+	public:
+	
+		static inline constexpr auto CONTROLLER_NAME = "JointPositionPDGravityController";
 
-        std::vector<std::string>
-            joint_names;
-        std::string              arm_id;
-        size_t                   num_joints = 7;
+		JointPositionPDGravityController() {}
+		~JointPositionPDGravityController() { sub_command.shutdown(); }
 
-        realtime_tools::RealtimeBuffer<std::vector<double>> commands_buffer;
+		bool 
+		init(hardware_interface::RobotHW *hw, ros::NodeHandle &nh) override;
 
-        JointPositionPDGravityController() {}
-        ~JointPositionPDGravityController() { sub_command.shutdown(); }
+		void 
+		starting(const ros::Time &time) override;
 
-        bool 
-        init(hardware_interface::RobotHW *hw, ros::NodeHandle &nh) override;
+		void 
+		update(const ros::Time &time, const ros::Duration &period) override;
 
-        void 
-        starting(const ros::Time &time) override;
+	private:
 
-        void 
-        update(const ros::Time &time, const ros::Duration &period) override;
+		std::string                                         arm_id;
+		size_t                                              num_joints;
+		std::vector<std::string>                            joint_names;
+		std::vector<hardware_interface::JointHandle>        joint_handles;
+		std::unique_ptr<franka_hw::FrankaModelHandle>       model_handle;
+		std::unique_ptr<franka_hw::FrankaStateHandle>       state_handle;
+		
+		Eigen::Matrix7d                                     Kp; // stiffness
+		Eigen::Matrix7d                                     Kd; // dampening
+		double                                              dtau_max;
+		
+		ros::Subscriber                                     sub_command;
+		realtime_tools::RealtimeBuffer<std::vector<double>> commands_buffer;
+		Eigen::Vector7d                                     q_d;
 
-    private:
+		Eigen::Vector7d
+		get_position();
 
-        ros::Subscriber sub_command;
+		Eigen::Vector7d
+		get_velocity();
 
-        Eigen::Vector7d q_d;
+		Eigen::Vector7d
+		get_gravity();
 
-        double kp = 200.0;
-        double kd = 100.0;
+		Eigen::Vector7d
+		saturate_rotatum(const Eigen::Vector7d &tau_des, const double period = 0.001 /* [s] */);
 
-        std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
-        std::unique_ptr<franka_hw::FrankaStateHandle> state_handle_;
-        std::vector<hardware_interface::JointHandle> joint_handles_;
-
-        Eigen::Vector7d
-        get_position();
-
-        Eigen::Vector7d
-        get_velocity();
-
-        Eigen::Vector7d
-        get_gravity();
-
-        Eigen::Matrix7x7
-        get_mass();
-
-        Eigen::Matrix6x7
-        get_jacobian();
-
-        Eigen::Vector7d
-        saturate_rotatum(const Eigen::Vector7d &tau_des, const double period = 0.001 /* [s] */);
-
-        void
-        callback_command(const std_msgs::Float64MultiArrayConstPtr &msg);
-    };
+		void
+		callback_command(const std_msgs::Float64MultiArrayConstPtr &msg);
+	};
 }
--- franka_controllers/src/joint_position_pd_gravity_controller.cpp
+++ franka_controllers/src/joint_position_pd_gravity_controller.cpp
@@ -1,227 +1,244 @@
 #include <franka_controllers/joint_position_pd_gravity_controller.h>
+
 #include <controller_interface/controller_base.h>
 #include <pluginlib/class_list_macros.hpp>
+
+// #include <ros_utils/ros.h>
 
 // export controller
 PLUGINLIB_EXPORT_CLASS(franka_controllers::JointPositionPDGravityController, controller_interface::ControllerBase)
 
 namespace franka_controllers
 {
-    
+
 bool
-JointPositionPDGravityController::init(hardware_interface::RobotHW *hw, ros::NodeHandle &nh)
+JointPositionPDGravityController::init(hardware_interface::RobotHW *hw, ros::NodeHandle& nh)
 {
 
-    std::string arm_id;
-    std::vector<std::string> joint_names;
+	// read arm_id from config file
+	if (not nh.getParam("arm_id", arm_id))
+	{
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Could not read parameter arm_id");
+		return false;
+	}
 
-    if (!nh.getParam("arm_id", arm_id))
-    { 
-        ROS_ERROR("franka_controller: Could not read parameter arm_id"); 
-        return false; 
-    }
+	// read joint_names from config file
+	if (not nh.getParam("joint_names", joint_names) || joint_names.size() != 7)
+	{
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Invalid or no joint_names parameters provided");
+		return false;
+	}
 
-    // instantiate the franka joint_names
-    if (!nh.getParam("joint_names", joint_names) || joint_names.size() != 7)
-    { 
-        ROS_ERROR( "franka_controller: Invalid or no joint_names parameters provided, aborting " "controller init!"); 
-        return false; 
-    }
-    
-    auto *model_interface = hw->get<franka_hw::FrankaModelInterface>();
+	// determine number of joints
+	if (num_joints = joint_names.size(); num_joints != 7)
+	{
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Invalid number of joints; got " << num_joints << ", expected " << 7);
+		return false;
+	}
 
-    // does the model_interface exist
-    if (model_interface == nullptr)
-    { 
-        ROS_ERROR_STREAM("franka_controller: Error getting model interface from hardware"); 
-        return false; 
-    }
+	// load model interface
+	auto* model_interface = hw->get<franka_hw::FrankaModelInterface>();
+	if (not model_interface)
+	{
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Error getting model interface from hardware");
+		return false;
+	}
 
-    try
-    { 
-        model_handle_ = std::make_unique<franka_hw::FrankaModelHandle>( model_interface->getHandle(arm_id + "_model"));
-    }
-    catch (hardware_interface::HardwareInterfaceException &ex)
-    { 
-        ROS_ERROR_STREAM( "franka_controller: Exception getting model handle from interface: " << ex.what());
-        return false;
-    }
+	// load model handle
+	try
+	{
+		model_handle = std::make_unique<franka_hw::FrankaModelHandle>(model_interface->getHandle(arm_id + "_model"));
+	}
+	catch (hardware_interface::HardwareInterfaceException& ex)
+	{
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Exception getting model handle from interface: " << ex.what());
+		return false;
+	}
 
-    auto *state_interface = hw->get<franka_hw::FrankaStateInterface>();
+	// load state interface
+	auto* state_interface = hw->get<franka_hw::FrankaStateInterface>();
+	if (not state_interface)
+	{
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Error getting state interface from hardware");
+		return false;
+	}
 
-    // does the state_interface exist 
-    if (state_interface == nullptr) 
-    { 
-        ROS_ERROR_STREAM("franka_controller: Error getting state interface from hardware"); 
-        return false; 
-    }
-    try
-    { 
-        state_handle_ = std::make_unique<franka_hw::FrankaStateHandle>(state_interface->getHandle(arm_id + "_robot")); 
-    }
-    catch (hardware_interface::HardwareInterfaceException &ex)
-    { 
-        ROS_ERROR_STREAM("franka_controller: Exception getting state handle from interface: " << ex.what()); 
-        return false;
-    }
+	// load state handle
+	try
+	{
+		state_handle = std::make_unique<franka_hw::FrankaStateHandle>(state_interface->getHandle(arm_id + "_robot"));
+	}
+	catch (hardware_interface::HardwareInterfaceException& ex)
+	{
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Exception getting state handle from interface: " << ex.what());
+		return false;
+	}
 
-    // does the effort_joint_interface exist
-    auto *effort_joint_interface = hw->get<hardware_interface::EffortJointInterface>();
+	// load effort joint interface
+	auto* effort_joint_interface = hw->get<hardware_interface::EffortJointInterface>();
+	if (not effort_joint_interface)
+	{
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Error getting effort joint interface from hardware");
+		return false;
+	}
 
-    if (effort_joint_interface == nullptr)
-    { 
-        ROS_ERROR_STREAM("franka_controller: Error getting effort joint interface from hardware");
-        return false; 
-    }
+	// load joint handles
+	for (const auto& joint_name : joint_names)
+	{
+		try
+		{
+			joint_handles.push_back(effort_joint_interface->getHandle(joint_name));
+		}
+		catch (const hardware_interface::HardwareInterfaceException& ex)
+		{
+			ROS_ERROR_STREAM(CONTROLLER_NAME << ": Exception getting joint handles: " << ex.what());
+			return false;
+		}
+	}
 
-    // fill the joint_handles_
-    for (size_t i = 0; i < num_joints; ++i)
-    {
-        try 
-        { 
-            joint_handles_.push_back(effort_joint_interface->getHandle(joint_names[i]));
-        }
-        catch (const hardware_interface::HardwareInterfaceException &ex) 
-        { 
-            ROS_ERROR_STREAM("franka_controller: Exception getting joint handles: " << ex.what()); 
-            return false; 
-        }
-    }
+	// read Kp gains from config
+	if (std::vector<double> vec_kp; nh.getParam("kp", vec_kp) and vec_kp.size() == num_joints)
+	{
+		Kp = Eigen::Vector7d(vec_kp.data()).asDiagonal();
+	}
+	else
+	{
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Could not read Kp gain");
+		return false;
+	}
+	
+	// read kd gains from config
+	if (std::vector<double> vec_kd; nh.getParam("kd", vec_kd) and vec_kd.size() == num_joints)
+	{
+		Kd = Eigen::Vector7d(vec_kd.data()).asDiagonal();
+	}
+	else
+	{
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Could not read Kd gain");
+		return false;
+	}
+	
+	// read dtau_max from config (-1 to disable)
+	if (not nh.getParam("dtau_max", dtau_max))
+	{
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Could not read dtau_max ");
+		return false;
+	}
+	
+	// read initial (home) joint position from config
+	if (std::vector<double> vec_q_d; nh.getParam("q_d", vec_q_d) and vec_q_d.size() == num_joints)
+	{
+		q_d = Eigen::Vector7d(vec_q_d.data());
+	}
+	else
+	{ 
+		ROS_ERROR_STREAM(CONTROLLER_NAME << ": Could not read q_d");
+		return false;
+	}
 
-    // initialize command buffer and q_d vector
-    commands_buffer.writeFromNonRT( std::vector<double>(num_joints, 0.0) ); // 7d vector filled with 0.0
+	// subscribe to command topic
+	sub_command = nh.subscribe<std_msgs::Float64MultiArray>("command", 1,& JointPositionPDGravityController::callback_command, this);
 
-    q_d = Eigen::Vector7d::Zero();
+	// init complete
+	ROS_INFO_STREAM("Initialized " << CONTROLLER_NAME << " with: "
+		<< "Kp = "       << Kp.diagonal().transpose() << ", "
+		<< "Kd = "       << Kd.diagonal().transpose() << ", "
+		<< "q_d = "      << q_d.transpose()  << ", "
+		<< "dtau_max = " << dtau_max
+	);
 
-    // subscribe to command "command" topic, such that we can control it from the terminal.
-    sub_command = nh.subscribe<std_msgs::Float64MultiArray>("command", 1, &JointPositionPDGravityController::callback_command, this);
-
-    std::cout << "sub_command.getTopic() : " << sub_command.getTopic() << std::endl;
-
-    // init complete
-    ROS_INFO_STREAM_NAMED(CONTROLLER_NAME, "Loaded " << CONTROLLER_NAME << " with kp = " << kp << ", kd = " << kd);
-    ROS_INFO_STREAM_NAMED(CONTROLLER_NAME," has been initialized successfully");
-
-    return true;
+	return true;
 }
 
-void 
-JointPositionPDGravityController::starting(const ros::Time &time)
+void
+JointPositionPDGravityController::starting(const ros::Time& time)
 {
-    // initial desired position
-    commands_buffer.writeFromNonRT(Q_D_INIT);
+	// initial desired position
+	std::vector<double> q_d_array(q_d.data(), q_d.data() + q_d.size()); // convert Eigen to std::vector
+	commands_buffer.writeFromNonRT(q_d_array);
 }
 
-void 
-JointPositionPDGravityController::update(const ros::Time &time, const ros::Duration &period)
+void
+JointPositionPDGravityController::update(const ros::Time& time, const ros::Duration& period)
 {
-    // elapsed time
-    static ros::Duration elapsed_time = ros::Duration(0.);
-    elapsed_time += period;
+	// elapsed time
+	static ros::Duration elapsed_time = ros::Duration(0.);
+	elapsed_time += period;
 
-    // get desired joint efforts
-    const std::vector<double> &commands = *commands_buffer.readFromRT();
+	// get desired joint efforts
+	const std::vector<double>& commands = *commands_buffer.readFromRT();
+	for (size_t i = 0; i < commands.size(); ++i)
+		q_d[i] = commands[i];
 
-    if (commands.empty())
-        return;
+	// read robot state
+	const auto q  = get_position();
+	const auto dq = get_velocity();
+	const auto g  = get_gravity();
 
-    for (size_t i = 0; i < commands.size(); ++i)
-        q_d[i] = commands[i];
+	// compute controller effort
+	Eigen::Vector7d tau_d = Kp * (q_d - q) - Kd * dq + g;
 
-    // read joint states
-    const auto q    = get_position();
-    const auto qdot = get_velocity();
+	// saturate rate-of-effort (rotatum)
+	if (dtau_max > 0)
+		tau_d = saturate_rotatum(tau_d, period.toSec());
 
-    const auto g = get_gravity();
-
-    // compute controller effort
-    Eigen::Vector7d tau_des = kp * (q_d - q) - kd * qdot + g;
-
-    // saturate rate-of-effort (rotatum)
-    if (SATURATE_ROTATUM)
-        tau_des = saturate_rotatum(tau_des, period.toSec());
-
-    // set desired command on joint handles
-    for (size_t i = 0; i < num_joints; ++i)
-        joint_handles_[i].setCommand(tau_des[i]);
+	// set desired command on joint handles
+	for (size_t i = 0; i < num_joints; ++i)
+		joint_handles[i].setCommand(tau_d[i]);
 }
 
 Eigen::Vector7d
 JointPositionPDGravityController::get_position()
 {
-    std::array<double, 7> q_array = state_handle_->getRobotState().q;
-    Eigen::Map<Eigen::Vector7d> Q(q_array.data());
-    return Q;
+	auto q_array = state_handle->getRobotState().q;
+	return Eigen::Vector7d(q_array.data());
 }
 
 Eigen::Vector7d
 JointPositionPDGravityController::get_velocity()
 {
-    std::array<double, 7> dq_array = state_handle_->getRobotState().dq;
-    Eigen::Map<Eigen::Vector7d> dQ(dq_array.data());
-    return dQ;
+	auto dq_array = state_handle->getRobotState().dq;
+	return Eigen::Vector7d(dq_array.data());
 }
 
 Eigen::Vector7d
 JointPositionPDGravityController::get_gravity()
 {
-    std::array<double, 7> gravity_array = model_handle_->getGravity();
-    Eigen::Map<Eigen::Vector7d> g(gravity_array.data());
-    return g;
-}
-
-Eigen::Matrix7x7
-JointPositionPDGravityController::get_mass()
-{
-    std::array<double, 49> m_array = model_handle_->getMass();
-    Eigen::Map<Eigen::Matrix7x7> m(m_array.data());
-    return m;
-}
-
-Eigen::Matrix6x7
-JointPositionPDGravityController::get_jacobian()
-{
-    std::array<double, 42> jacobian_array = model_handle_->getZeroJacobian(franka::Frame::kEndEffector);
-    Eigen::Map<Eigen::Matrix6x7> jacobian(jacobian_array.data());
-    return jacobian;
+	auto g_array = model_handle->getGravity();
+	return Eigen::Vector7d(g_array.data());
 }
 
 Eigen::Vector7d
-JointPositionPDGravityController::saturate_rotatum(const Eigen::Vector7d &tau_des, const double period)
+JointPositionPDGravityController::saturate_rotatum(const Eigen::Vector7d& tau_d, const double period)
 {
-    // previous desired torque and saturated torque
-    static Eigen::Vector7d tau_des_prev = Eigen::Vector7d::Zero();
-    static Eigen::Vector7d tau_des_sat = Eigen::Vector7d::Zero();
+	// previous desired torque and saturated torque
+	static Eigen::Vector7d tau_d_prev = Eigen::Vector7d::Zero();
+	static Eigen::Vector7d tau_d_sat = Eigen::Vector7d::Zero();
 
-    // compute saturated torque
-    for (size_t i = 0; i < tau_des_sat.size(); ++i)
-    {
-        const double tau_dot = (tau_des[i] - tau_des_prev[i]) / period;
-        tau_des_sat[i] = tau_des_prev[i] + std::max(std::min(tau_dot, TAU_DOT_MAX * period), -(TAU_DOT_MAX * period));
-    }
+	// compute saturated torque
+	for (size_t i = 0; i < tau_d_sat.size(); ++i)
+	{
+		const double dtau = (tau_d[i] - tau_d_prev[i]) / period;
+		tau_d_sat[i] = tau_d_prev[i] + std::max(std::min(dtau, dtau_max * period), -(dtau_max * period));
+	}
 
-    // save for next iteration and return
-    tau_des_prev = tau_des_sat;
-    return tau_des_sat;
+	// save for next iteration and return
+	tau_d_prev = tau_d_sat;
+	return tau_d_sat;
 }
 
-void 
-JointPositionPDGravityController::callback_command(const std_msgs::Float64MultiArrayConstPtr &msg)
+void
+JointPositionPDGravityController::callback_command(const std_msgs::Float64MultiArrayConstPtr& msg)
 {
+	// check size
+	if (msg->data.size() != num_joints)
+	{
+		ROS_ERROR_NAMED(CONTROLLER_NAME, "Number of desired values in command (%lu) does not match number of joints (%lu); execution aborted.", msg->data.size(), num_joints);
+		return;
+	}
 
-    if (msg->data.empty())
-        ROS_WARN("Data was empty %ld",msg->data.size());
-    else
-        ROS_WARN("Data size: %ld", msg->data.size());
-
-    // check size
-    if (msg->data.size() != num_joints)
-    {
-        ROS_ERROR_NAMED(CONTROLLER_NAME, "Number of desired values in command (%lu) does not match number of joints (%lu); execution aborted.", msg->data.size(), num_joints);
-        return;
-    }
-    // write commands to command buffer
-    commands_buffer.writeFromNonRT(msg->data);
+	// write commands to command buffer
+	commands_buffer.writeFromNonRT(msg->data);
 }
 
 } // namespace franka_controllers
