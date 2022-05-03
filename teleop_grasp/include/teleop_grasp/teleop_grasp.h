c362095d103538787444b081a09580af895327f0
Victor Staven <victorstaven@protonmailcom>
2022-05-03 22:03:35 +0200

    can follow, but problem with pose estimate node, node dies

--- teleop_grasp/include/teleop_grasp/teleop_grasp.h
+++ teleop_grasp/include/teleop_grasp/teleop_grasp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Eigen/Eigen>
+#include "Eigen/src/Geometry/Transform.h"
 #include "geometry_msgs/Pose.h"
 #include <array>
 #include <franka_gripper/franka_gripper.h>
@@ -25,7 +26,7 @@ namespace teleop_grasp
 	inline std::array<geometry_msgs::Pose, 3> poses_prev;
 	inline constexpr int                      MAX_LIN_VELOCITY = 1;
 	inline constexpr double                   MAX_ANG_VELOCITY = M_PI / 2.0;
-	inline constexpr double                   CHANGE_THRESHOLD = 1e-6;
+	inline constexpr double                   CHANGE_THRESHOLD = 0.05;
 
 	void
 	calibrate();
@@ -48,4 +49,29 @@ namespace teleop_grasp
 		geometry_msgs::Pose
 		predict_pose_linear();
 	}
+
+	namespace utils
+	{
+		geometry_msgs::Pose 
+		get_current_hand_pose();
+
+		geometry_msgs::Pose
+		get_current_franka_pose();
+
+		void 
+		set_current_franka_pose(const geometry_msgs::Pose& des_pose);
+
+		Eigen::Isometry3d
+		rotate_to_hand_frame(Eigen::Isometry3d tf);
+
+		bool 
+		has_any_change_occurred(const Eigen::Isometry3d& pose1_tf, const Eigen::Isometry3d& pose2_tf);
+
+		bool
+		has_too_much_change_occurred(const Eigen::Isometry3d& pose1_tf,const Eigen::Isometry3d& pose2_tf);
+	}
+
+
+
+
 }
--- teleop_grasp/src/teleoperation.cpp
+++ teleop_grasp/src/teleoperation.cpp
@@ -4,6 +4,7 @@
 #include "ros/param.h"
 #include "ros/rate.h"
 #include "ros/subscriber.h"
+#include "ros_utils/eigen.h"
 #include "std_msgs/Bool.h"
 #include "teleop_grasp/teleop_grasp.h"
 #include <cstddef>
@@ -23,16 +24,30 @@ main(int argc, char** argv)
 	auto topic_franka_pose_ee   = ros::param::param<std::string>("~topic_franka_pose_ee","");
 
 	ros::Subscriber sub_grasp = nh.subscribe<std_msgs::Bool>( topic_grasp_state, 1, [&](const auto& msg){ teleop_grasp::gesture_state = (*msg).data; });
-	ros::Subscriber sub_pose  = nh.subscribe<geometry_msgs::Pose>( topic_pose_hand, 1, [&](const auto& msg){ teleop_grasp::pose_hand = *msg; }); 
+	ros::Subscriber sub_pose  = nh.subscribe<geometry_msgs::Pose>( topic_pose_hand, 1, [&](const auto& msg){ teleop_grasp::pose_hand = *msg;ROS_WARN_STREAM("hand pose cb: \n" << teleop_grasp::pose_hand.position); }); 
 
-	std::cin.get();
+	// std::cin.get();
 	teleop_grasp::calibrate();
 
 	ros::Rate rate(10);
 
+	auto const_franka_pos = teleop_grasp::utils::get_current_franka_pose().position;
+	const_franka_pos.x += 0.2;
+
+
+	auto hand = teleop_grasp::utils::get_current_hand_pose();
+	hand.position = teleop_grasp::utils::get_current_franka_pose().position;
+	hand.position.x += 0.2;
+	auto hand_tf = teleop_grasp::utils::rotate_to_hand_frame( Eigen::make_tf( hand ));
+	hand = geometry_msgs::make_pose(hand_tf);
+
 	while (ros::ok()) 
 	{
-
+		// hand = teleop_grasp::utils::get_current_hand_pose();
+		// hand_tf = teleop_grasp::utils::rotate_to_hand_frame( Eigen::make_tf( hand ));
+		// hand = geometry_msgs::make_pose(hand_tf);
+		// hand.position = const_franka_pos;
+		// teleop_grasp::utils::set_current_franka_pose(hand);
 		// open and close gripper
 		auto gripper_state = (teleop_grasp::gesture_state) ? teleop_grasp::GripperState::OPEN : teleop_grasp::GripperState::CLOSE;
 		teleop_grasp::command_gripper( gripper_state );
@@ -40,12 +55,13 @@ main(int argc, char** argv)
 		// predict the next position of hand
 		auto pose_ee_pred = teleop_grasp::predictor::predict_pose_linear();
 
+		ROS_WARN_STREAM("hand pose received: \n" << teleop_grasp::pose_hand.position);
+
 		// compute the new position based on relative difference between hand poses
 		teleop_grasp::pose_ee_des = teleop_grasp::compute_desired_ee_pose( teleop_grasp::pose_hand );
 
 		// send pose to franka | choose between predicted pose, and desired pose
 		teleop_grasp::command_pose_robot( teleop_grasp::pose_ee_des );
-		// teleop_grasp::command_pose_robot( teleop_grasp::pose_ee_des, topic_franka_pose_ee );
 
 		ros::spinOnce();
 		rate.sleep();
